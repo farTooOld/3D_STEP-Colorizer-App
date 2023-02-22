4aef87ca09bfd87ffede27cd22fb639365b6280c
Bruce <23279806+farTooOld@users.noreply.github.com>
2023-02-21 17:13:35 -0800

    Add files via upload

---
+++ STEP_Colorizer.py
@@ -0,0 +1,25 @@
+import sys
+import pcbnew
+import os
+import subprocess
+import wx
+
+pcbnew.GetBoard()
+from pcbnew import *
+
+class SimplePlugin(pcbnew.ActionPlugin):
+    
+    def defaults(self):
+        self.name = "STEP_Colorizer"
+        self.category = "PCB_Parts"
+        self.description = "STEP_Color_Changer"
+        self.show_toolbar_button = True # Optional, defaults to False
+        self.icon_file_name = os.path.join(os.path.dirname(__file__), 'k_STEPcolor.png') # Optional, defaults to ""
+
+    def Run(self):
+        pass  # a Null command until there's executable code here...
+  # This next line works from Kicad!!!!!! plugin
+        subprocess.call(['/usr/bin/open','/Users/bruce/dist/STEP_Colorizer.app'])
+
+SimplePlugin().register() # Instantiate and register to Pcbnew
+
